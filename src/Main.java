6598b9124bb232a3f57462b54b9fb0f09162046c
ikostan <superegorius@gmail.com>
2017-04-29 02:42:54 -0700

    Pre-final version

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+//import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.stream.DoubleStream;
+//import java.lang.reflect.Array;
+//import java.util.ArrayList;
 
 public class Main {
 
@@ -27,6 +28,15 @@ public class Main {
 	private final static String FILE = "Environmental_Data_Deep_Moor_2015.txt";
 	private final static int firstRow = 0;
 	
+	private static Double[] wind_speed;
+	private static Double total_wind_speed;
+	
+	private static Double[] air_temperature;
+	private static Double total_air_temperature;
+	
+	private static Double[] barometric_pressure;
+	private static Double total_barometric_pressure;
+	
 	//MEAN
 	private static double mean_wind_speed;
 	private static double mean_air_temperature;
@@ -36,32 +46,65 @@ public class Main {
 	private static double median_wind_speed;
 	private static double median_air_temperature;
 	private static double median_barometric_pressure;
-	
-	//
 
 	
 	//Main method
 	public static void main(String[] args) throws IOException {
-		
-		
-		Double[] wind_speed;
-		Double[] air_temperature;
-		Double[] barometric_pressure;
 			
-		
 		int totalRows = totalLines(FILE) - 1;
 		
 		wind_speed = new Double[totalRows];
 		air_temperature = new Double[totalRows];
 		barometric_pressure = new Double[totalRows];
-						
-		readSource(FILE);
-		
+			
+		readSource(FILE, wind_speed, air_temperature, barometric_pressure);
+		clacMean();
 
+		
+		median_wind_speed = calcMedian(wind_speed);
+		median_air_temperature = calcMedian(air_temperature);
+		median_barometric_pressure = calcMedian(barometric_pressure);
+		
+		System.out.println(
+				String.format(
+						"median_wind_speed: %.2f, "
+						+ "median_air_temperature: %.2f, "
+						+ "median_barometric_pressure: %.2f", 
+						median_wind_speed, median_air_temperature, median_barometric_pressure));
 		
 	}
 	
+	//Calculate and MEDIAN
+	private static double calcMedian(Double[] dataArray){
+		
+		double median = 0.0;
+		int index = 0;
+		
+		if((dataArray.length % 2) == 0){
+			
+			median = (dataArray[dataArray.length] + dataArray[dataArray.length + 1]) / 2;		
+		}
+		else{
+			index = dataArray.length / 2;			
+			median = dataArray[index];
+		}
+		
+		return median;
+	}
 	
+	
+	//Calculate and display MEAN values
+	private static void clacMean(){
+		
+		mean_wind_speed = total_wind_speed  / wind_speed.length;
+		mean_air_temperature = total_air_temperature / air_temperature.length;
+		mean_barometric_pressure = total_barometric_pressure / barometric_pressure.length;
+		
+		System.out.println(String.format("mean_air_temperature: %.2f, mean_air_temperature: %.2f, mean_barometric_pressure: %.2f", mean_wind_speed, mean_air_temperature, mean_barometric_pressure));
+	}
+	
+	
+	//Get total number of lines in the source file
 	private static int totalLines(String FILE) throws IOException{
 		
 		FileReader fileReader = new FileReader(FILE);
@@ -83,14 +126,19 @@ public class Main {
 				System.out.println(e.getMessage());
 			}
 		}
+		
 		fileReader.close();
-		System.out.println(i);
 		return i;
 	}
 	
 	
-	private static void readSource(String FILE) throws IOException{
+	//Read source file and extract rellevant data from it
+	private static void readSource(String FILE, Double[] wind_speed, Double[] air_temperature, Double[] barometric_pressure) throws IOException{
 		
+		total_wind_speed = 0.0;
+		total_air_temperature = 0.0;
+		total_barometric_pressure = 0.0;
+				
 		FileReader fileReader = new FileReader(FILE);
 		BufferedReader bufReader = new BufferedReader(fileReader);
 		
@@ -106,17 +154,20 @@ public class Main {
 				
 				if(row != firstRow && line != null){
 							
-					//dataLine = line.split("");
-					System.out.println(line);
-					//System.out.println(line.split(" "));
+					dataLine = line.split("\t");					
+					index = row - 1; //Index for array members
 					
-					index = row - 1;
-					//wind_speed[index] = Double.parseDouble(dataLine[8]);
-					//air_temperature[index] = Double.parseDouble(dataLine[2]);
-					//barometric_pressure[index] = Double.parseDouble(dataLine[3]);
+					wind_speed[index] = Double.parseDouble(dataLine[7]);
+					total_wind_speed = total_wind_speed + wind_speed[index];
+					
+					air_temperature[index] = Double.parseDouble(dataLine[1]);
+					total_air_temperature = total_air_temperature + air_temperature[index];
+					
+					barometric_pressure[index] = Double.parseDouble(dataLine[2]);
+					total_barometric_pressure = total_barometric_pressure + barometric_pressure[index];
 				}
-				System.out.println(row);
-				row++;
+				
+				row++; //Increment
 			}
 		} catch (IOException ex) {
 			// TODO Auto-generated catch block
